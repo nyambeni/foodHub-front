8387db3cc03986d5079c9b6c3f43686854f9cf5a
tibitig <maila.mtebello@gmail.com>
2021-02-18 15:08:36 +0200

    added menu icon on modified pages, changed home page layout to include product images

--- src/app/pages/cart-modal/cart-modal.page.ts
+++ src/app/pages/cart-modal/cart-modal.page.ts
@@ -52,8 +52,12 @@
 
 import { Product, CartService } from '../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
-import { ModalController, AlertController } from '@ionic/angular';
+
+
+import { NavController, NavParams, AlertController, ModalController, ActionSheetController , LoadingController} from '@ionic/angular';
 import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
+import { SearchService } from '../../services/search.service'
 
 @Component({
   selector: 'app-cart-modal',
@@ -64,9 +68,124 @@ export class CartModalPage implements OnInit {
 
   cart: Product[] = [];
 
+  contentLoaded = false;
   // tslint:disable-next-line: max-line-length
-  constructor(private cartService: CartService, private modalCtrl: ModalController, private alertCtrl: AlertController, private router: Router) { }
+  constructor(private cartService: CartService, private modalCtrl: ModalController, private alertCtrl: AlertController, private router: Router,
+    private route: Router, public actionSheetController: ActionSheetController, private loadingCtrl: LoadingController, private dataService: DataService,
+               private searchService: SearchService) {
+                setTimeout(() => {
+                  this.contentLoaded = true;
+                }, 3000);
+                }
 
+                async presentActionSheet() {
+                  const actionSheet = await this.actionSheetController.create({
+                    header: 'Menu',
+                    buttons: [{
+                      text: 'Home',
+                      role: 'destructive',
+                      icon: 'home-outline',
+                      handler: () => {
+                        console.log('Home clicked');
+                        this.route.navigateByUrl('home');
+                      }
+                    }, {
+                      text: 'My Account',
+                      icon: 'person-circle-outline',
+                      handler: () => {
+                        console.log('Myprofile clicked');
+                        this.route.navigateByUrl('/custprof');
+                      }
+                    }, {
+                      text: 'Restaurants',
+                      icon: 'restaurant-outline',
+                      handler: () => {
+                        console.log('Restaurants clicked');
+                        this.route.navigateByUrl('/restaurants');
+                      }
+                    }, {
+                      text: 'My cart',
+                      icon: 'cart-outline',
+                      handler: () => {
+                        console.log('My cart clicked');
+                        this.route.navigateByUrl('/cart-modal');
+                      }
+                    },
+                     {
+                      text: 'Signup',
+                      icon: 'person-add-outline',
+                      handler: () => {
+                        console.log('Signup clicked');
+                        this.route.navigateByUrl('/signup');
+                      }
+                     },
+                     {
+                       text: 'Order',
+                       icon: 'time-outline',
+                       handler: () => {
+                         console.log('Share clicked');
+                         this.route.navigateByUrl('/order2');
+                       }
+                     },
+                   {
+                    text: 'login',
+                    icon: 'person',
+                    handler: () => {
+                      console.log('login clicked');
+                      this.route.navigateByUrl('/login');
+                    }
+                },
+                    {
+                     text: 'History',
+                     icon: 'time-outline',
+                     handler: () => {
+                       console.log('Share clicked');
+                       this.route.navigateByUrl('/profile');
+                     }
+                   }, {
+                     text: 'Support',
+                     icon: 'help-circle-outline',
+                     handler: () => {
+                       console.log('Share clicked');
+                       this.route.navigateByUrl('/profile');
+                     }
+                   }, {
+                     text: 'About Us',
+                     icon: 'information-circle-outline',
+                     handler: () => {
+                       console.log('Share clicked');
+                       this.route.navigateByUrl('/profile');
+                     }
+                   },
+                      {
+                       text: 'goto admin',
+                       icon: 'help-circle-outline',
+                       handler: () => {
+                         console.log('admin clicked');
+                         this.route.navigateByUrl('/vendor-admin');
+                       }
+                   }
+                   ,
+                      {
+                       text: 'goto superadmin',
+                       icon: 'help-circle-outline',
+                       handler: () => {
+                         console.log('super admin clicked');
+                         this.route.navigateByUrl('/super-admin');
+                       }
+                   }, {
+                      text: 'Cancel',
+                      icon: 'close',
+                      role: 'cancel',
+             
+                      handler: () => {
+                        console.log('Cancel clicked');
+                      }
+                    }]
+                  });
+                  await actionSheet.present();
+                }
+                
   ngOnInit() {
     this.cart = this.cartService.getCart();
   }
