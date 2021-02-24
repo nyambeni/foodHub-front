43207efe9fb6c868b6aca98d5ae9293833b22579
Dimpho <dimphomothibe003@gmail.com>
2021-02-24 15:31:02 +0200

    Validation update

--- src/app/pages/singupadmin/singupadmin.page.ts
+++ src/app/pages/singupadmin/singupadmin.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import {PostProvider } from '../../../providers/post-provider';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, Validators, FormGroup, AbstractControl, FormControl, ValidatorFn } from '@angular/forms';
+
 
 @Component({
   selector: 'app-singupadmin',
@@ -10,81 +12,29 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class SingupadminPage implements OnInit {
 
-    // tslint:disable-next-line: variable-name
-    restuarant_id: number;
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: no-inferrable-types
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: quotemark
-    // tslint:disable-next-line: no-inferrable-types
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: no-inferrable-types
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: no-inferrable-types
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: no-inferrable-types
-    // tslint:disable-next-line: variable-name
-    // tslint:disable-next-line: quotemark
-    restuarant_name: string = "";
-    contact: string = "";
-    address_loc: string = "";
-    pass_word: string = "";
-    email_address: string = "";
-    rest_status: string = "";
-    conPassword: string = "";
+  //vendorReg : FormGroup;
 
   constructor(
     private router: Router,
     private postPvdr: PostProvider,
     private actRoute:ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb : FormBuilder) { }
+
+    vendorReg = this.fb.group({
+      restuarant_name: ['', Validators.required],
+      email: ['', Validators.required],
+      cellPhone: ['', Validators.required],
+      address: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    })
 
   ngOnInit() {
 
-    this.actRoute.params.subscribe((data: any) => {
-
-      this.restuarant_id = data.id;
-      this.restuarant_name = data.name;
-      this.pass_word = data.password;
-      this.email_address = data.e_address;
-      this.rest_status = data.status;
-      this.contact = data.phone;
-      this.address_loc = data.address;
-      this.conPassword = data.conP;
-      console.log(data);
-
-    });
-
   }
 
-  goHome() {
-
-      return new Promise(resolve => {
-
-        const body = {
-
-          aksi: 'add-restInfo',
-          restuarant_name: this.restuarant_name,
-          pass_word: this.pass_word,
-          email_address: this.email_address,
-          rest_status: this.rest_status,
-          contact: this.contact,
-          address_loc: this.address_loc,
-          conPassword: this.conPassword,
-
-        };
-        this.postPvdr.postData(body, 'proses-api.php').subscribe(data => {
-        this.router.navigateByUrl('/login');
-        console.log('submit works');
-
-        });
-
-      });
-
-      this.router.navigate(['home']);
-
- }
+ 
  navAdmin() {
   return this.router.navigateByUrl('/vendor-admin');
 }
@@ -96,6 +46,10 @@ backButton() {
   this.location.back();
  }
 
+
+ logVendor() {
+   console.log(this.vendorReg.value);
+ }
 
 }
 
